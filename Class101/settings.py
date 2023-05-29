fbd12c5db8632cc9a5ce064df32721a8fc0fbfcc
bilal Patiwala <bilalpatiwala04@gmail.com>
2023-05-29 21:58:52 +0530

    changes

--- Class101/settings.py
+++ Class101/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'Class101.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASS"),
--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
+python3.9 manage.py migrate auth
+python manage.py migrate --run-syncdb
 python3.9 manage.py collectstatic
